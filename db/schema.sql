1038a9989dbfc3c4084491fa796638b280b2eb4c
Jakob A <jdanders1999@gmail.com>
2023-01-26 20:33:54 -0500

    Added roles table in shcema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,12 @@ CREATE TABLE departments (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    job_title VARCHAR(30) NOT NULL,
+    salary INT NOT NULL,
+    departments_id INT NOT NULL,
+    FOREIGN KEY (departments_id) REFERENCES departments(id)
+);
+
