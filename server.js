41d4e274d481d44d119c32245168a4ad1b70a25a
Jakob A <jdanders1999@gmail.com>
2023-01-26 21:58:23 -0500

    Set up else if user selects to view employees

--- server.js
+++ server.js
@@ -21,17 +21,25 @@ function startPrompt() {
         choices: ['View Departments', 'View Roles', 'View Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'None']
     })
         .then((answer) => {
-            if (answer.task == 'View Departments') {
+            if(answer.task == 'View Departments') {
                 db.query('SELECT * FROM departments', function (err, res) {
                     console.table(res)
                     if (err) throw err
                 })
-            } else if () {
-
-            }
-        })
-
         
+            } else if(answer.task == 'View Roles') {
+                db.query('SELECT * FROM roles', function (err, res) {
+                    console.table(res)
+                    if (err) throw err
+                })
+            } else if(answer.task == 'View Employees') {
+                db.query('SELECT * FROM employees', function (err, res) {
+                    console.table(res)
+                    if (err) throw err
+                })
+            }
+        
+    })    
 };
 
 startPrompt();
