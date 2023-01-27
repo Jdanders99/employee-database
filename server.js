da945e53034ae1f6ed9c74dbf1419213dbecc4fe
Jakob A <jdanders1999@gmail.com>
2023-01-26 21:53:44 -0500

    Added first prompt for user selection, viewing departments or roles

--- server.js
+++ server.js
@@ -1,23 +1,37 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+const consoleTable = require('console.table');
 const PORT = process.env.PORT || 3001;
 
 const db = mysql.createConnection(
     {
-        host:'localhost',
-        user:'root',
-        password:'pw5253',
-        database:'employee_db'
+        host: 'localhost',
+        user: 'root',
+        password: 'pw5253',
+        database: 'employee_db'
     },
+    console.log('Connected to the employee db')
 );
 
-function startPrompt () {
+function startPrompt() {
     inquirer.prompt({
         type: 'list',
         name: 'task',
         message: 'What would you like to do?',
         choices: ['View Departments', 'View Roles', 'View Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'None']
     })
-}
+        .then((answer) => {
+            if (answer.task == 'View Departments') {
+                db.query('SELECT * FROM departments', function (err, res) {
+                    console.table(res)
+                    if (err) throw err
+                })
+            } else if () {
+
+            }
+        })
+
+        
+};
 
 startPrompt();
