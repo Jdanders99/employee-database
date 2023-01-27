40686541315a37e52d0757c199cdf5bbf15184ea
Jakob A <jdanders1999@gmail.com>
2023-01-26 20:30:26 -0500

    Created departments table for employee_db

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,8 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
 USE employee_db;
+
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
+);
