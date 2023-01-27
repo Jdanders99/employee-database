883f9b802dcc1a38a04c7733ce0d8f29b279775f
Jakob A <jdanders1999@gmail.com>
2023-01-26 20:56:06 -0500

    Added inserts for employees into seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -16,4 +16,15 @@ INSERT INTO roles (departments_id, job_title, salary)
     (1, 'Lawyer', 150000),
     (1, 'Principle Attorney', 200000),
     (1, 'Salesperson', 60000),
-    (1, 'Head of Sales', 100000),
+    (1, 'Head of Sales', 100000);
+
+INSERT INTO employees (manager_id, roles_id, first_name, last_name)
+    VALUES
+    (null, 1, 'Mike', 'Tyson'),
+    (1, 2, 'Jane', 'Higgins'),
+    (null, 3, 'Zac', 'Brown'),
+    (7, 4, 'Johnny', 'Depp'),
+    (null, 5, 'Mariah', 'Carey'),
+    (3, 6, 'John', 'Deer'),
+    (null, 7, 'Oliver', 'Queen'),
+    (4, 8, 'Lebron', 'James'),
