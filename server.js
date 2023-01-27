bb956f4c07bd5cbb4c37ea9e1255b4d7dd5f28d7
Jakob A <jdanders1999@gmail.com>
2023-01-26 21:07:30 -0500

    Set up connection with db in server.js

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+
+const db = mysql.createConnection(
+    {
+        host:'localhost',
+        user:'root',
+        password:'pw5253',
+        database:'employee_db'
+    },
+);
